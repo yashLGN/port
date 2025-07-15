6217acffca07894ec6420778902c913c2369e9ba
Deepyash <158832695+yashLGN@users.noreply.github.com>
2025-07-15 15:16:36 +0530

    github, youtube channel link attached

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import Terminal from './terminal.jsx'
+import Github_buttons from './github_buttons.jsx'
 
 function App() {
   return (
+    <>
     <div className="terminal-div">
       <Terminal />
     </div>
+    <div className="github-buttons">
+      <Github_buttons/>
+    </div>
+    </>
   );
 }
 
---
+++ frontend/src/github_buttons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Github_buttons() {
+  return (
+    <div className="github-buttons">
+    <a type="button" href='https://github.com/yashLGN' class="btn btn-lg me-2 github-button">Github</a>
+    <a type="button" href='https://www.youtube.com/@Lgn7733' class="btn btn-outline-danger btn-lg ms-2">Music Channel</a>
+    </div>
+  )
+}
